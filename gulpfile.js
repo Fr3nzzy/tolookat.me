0b0bb972317d35cfb5228eaf6b85d80d6a9cf10d
Dmitry Ananichev <a@qozz.ru>
2015-09-26 19:42:29 +0500

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var minifyCSS = require('gulp-minify-css');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var defaultTask = ['compile-css'];
+
+gulp.task('compile-css', ['clean:css'], function() {
+  return gulp.src(['styles/less/tolookatme.less'])
+    .pipe(sourcemaps.init())
+    .pipe(less({
+      paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('styles/css'));
+});
+
+gulp.task('minify-css', ['clean:css'], function() {
+  return gulp.src(['styles/less/tolookatme.less'])
+    .pipe(less({
+      paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(sourcemaps.init())
+    .pipe(minifyCSS())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('styles/css'))
+    .pipe(gulp.dest('styles/css'));
+});
+
+gulp.task('clean:css', function(cb) {
+  del([
+    'styles/css/**/*'
+  ], cb);
+});
+
+gulp.task('build', ['minify-css']);
+
+gulp.task('default', defaultTask);
